6b6fc22f4b94092a1be438722963f3d6c13f5bc2
Yasmine Subbagh <ysubbagh@gmail.com>
2024-10-22 12:38:07 -0700

    clustiern code work and files for notebook support being made

--- Flavor-Network-master/code/recipe_clustering.py
+++ Flavor-Network-master/code/recipe_clustering.py
@@ -15,7 +15,7 @@ from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.models import HoverTool
 import nltk
 nltk.data.path.append('/Users/yasminesubbagh/nltk_data')
-nltk.download()
+# nltk.download()
 
 #take some regional cuisines, tsne clustering, and plotting
 def tsne_cluster_cuisine(df,sublist):
@@ -30,7 +30,8 @@ def tsne_cluster_cuisine(df,sublist):
     print(df_X.shape, lenlist)
 
     dist = squareform(pdist(df_X, metric='cosine'))
-    tsne = TSNE(metric='precomputed').fit_transform(dist)
+    tsne = TSNE(metric='precomputed', init='random').fit_transform(dist)
+
 
     palette = sns.color_palette("hls", len(sublist))
     plt.figure(figsize=(10,10))
@@ -40,42 +41,48 @@ def tsne_cluster_cuisine(df,sublist):
     plt.legend()
 
 #interactive plot with boken; set up for four categories, with color palette; pass in df for either ingredient or flavor
-def plot_bokeh(df,sublist,filename):
-    lenlist=[0]
-    df_sub = df[df['cuisine']==sublist[0]]
+def plot_bokeh(df, sublist, filename):
+    lenlist = [0]
+    df_sub = df[df['cuisine'] == sublist[0]]
     lenlist.append(df_sub.shape[0])
     for cuisine in sublist[1:]:
-        temp = df[df['cuisine']==cuisine]
-        df_sub = pd.concat([df_sub, temp],axis=0,ignore_index=True)
+        temp = df[df['cuisine'] == cuisine]
+        df_sub = pd.concat([df_sub, temp], axis=0, ignore_index=True)
         lenlist.append(df_sub.shape[0])
-    df_X = df_sub.drop(['cuisine','recipeName'],axis=1)
+    df_X = df_sub.drop(['cuisine', 'recipeName'], axis=1)
     print(df_X.shape, lenlist)
 
     dist = squareform(pdist(df_X, metric='cosine'))
-    tsne = TSNE(metric='precomputed').fit_transform(dist)
-    #cannot use seaborn palette for bokeh
-    palette =['red','green','blue','yellow']
-    colors =[]
+    tsne = TSNE(metric='precomputed', init='random').fit_transform(dist)
+
+    # Define colors for each cuisine
+    palette = ['red', 'green', 'blue', 'yellow']
+    colors = []
     for i in range(len(sublist)):
-        for j in range(lenlist[i+1]-lenlist[i]):
+        for j in range(lenlist[i+1] - lenlist[i]):
             colors.append(palette[i])
-    #plot with boken
+
+    # Set up Bokeh plot
     output_file(filename)
-    source = ColumnDataSource(
-            data=dict(x=tsne[:,0],y=tsne[:,1],
-                cuisine = df_sub['cuisine'],
-                recipe = df_sub['recipeName']))
+    source = ColumnDataSource(data=dict(
+        x=tsne[:, 0],  # x-values from TSNE
+        y=tsne[:, 1],  # y-values from TSNE
+        cuisine=df_sub['cuisine'],
+        recipe=df_sub['recipeName'],
+        fill_color=colors  # Colors corresponding to cuisines
+    ))
 
     hover = HoverTool(tooltips=[
-                ("cuisine", "@cuisine"),
-                ("recipe", "@recipe")])
+        ("cuisine", "@cuisine"),
+        ("recipe", "@recipe")
+    ])
 
-    p = figure(plot_width=1000, plot_height=1000, tools=[hover],
+    p = figure(width=1000, height=1000, tools=[hover],
                title="flavor clustering")
 
-    p.circle('x', 'y', size=10, source=source,fill_color=colors)
-
+    p.circle('x', 'y', size=10, source=source, fill_color='fill_color')
     show(p)
+
 
 
 if __name__ == '__main__':
---
+++ shell_files/run_clustering.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run the recipe clustering Python script
+
+# Ensure you are in the correct directory
+python3 ../Flavor-Network-master/code/recipe_clustering.py
